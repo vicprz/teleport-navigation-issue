a933a45ee6ee894524709f1f94b82564c13f4bf5
Victor Perez <victor.perez@theodo.com>
2026-01-07 12:22:49 +0100

    setup react-native-teleport

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { PortalProvider } from 'react-native-teleport';
 
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-        <Stack />
-    </SafeAreaProvider>
+    <PortalProvider>
+        <SafeAreaProvider>
+            <Stack />
+        </SafeAreaProvider>
+    </PortalProvider>
   );
 }
